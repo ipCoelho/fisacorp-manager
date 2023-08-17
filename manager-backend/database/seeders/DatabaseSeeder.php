3aa5dd22344e697e0b1ff2f191dd83b18d2bb4d9
Israel Coelho <ipcoelho.business@gmail.com>
2023-08-17 13:30:51 -0300

    feat(seeds): Class to manage and create seeds (mock-data) added

--- manager-backend/database/seeders/DatabaseSeeder.php
+++ manager-backend/database/seeders/DatabaseSeeder.php
@@ -2,36 +2,51 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB; // Import the DB facade
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // Check if records already exist in the 'users' table
         $existingUsersCount = DB::table('users')->count();
+        $existingTasksCount = DB::table('tasks')->count();
 
         if ($existingUsersCount === 0) {
-            // Insert two records into the 'users' table
             DB::table('users')->insert([
                 [
                     'username' => 'admin',
                     'email' => 'admin@admin.com',
-                    'password' => bcrypt('root'), // Use bcrypt() to hash the password
+                    'password' => bcrypt('admin'),
+                    'created_at' => now(),
+                    'updated_at' => now(),   
                 ],
                 [
-                    'username' => 'israel',
-                    'email' => 'israel@fisacorp.com',
+                    'username' => 'fulano',
+                    'email' => 'fulano@fisacorp.com',
                     'password' => bcrypt('12345678'),
+                    'created_at' => now(),
+                    'updated_at' => now(),
                 ],
             ]);
+        }
+
+        if ($existingTasksCount === 0) {
+            $users = DB::table('users')->take(2)->get();
+
+            $tasksToInsert = [];
+            foreach ($users as $i => $user) {
+                $tasksToInsert[] = [
+                    'user_id' => $user->id,
+                    'title' => 'Tarefa nº' . $i . ' de ' . $user->username,
+                    'fulfilled' => false,
+                    'description' => 'Descrição da tarefa de nº '  . $i . ' de ' . $user->username,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+            }
+
+            DB::table('tasks')->insert($tasksToInsert);
         }
     }
 }
