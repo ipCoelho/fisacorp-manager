9510a8b56dc6e58e8e7131d33a3ce0463333eb8c
Israel Coelho <ipcoelho.business@gmail.com>
2023-08-17 13:30:11 -0300

    feat(migration): Table 'tasks' migration added

---
+++ manager-backend/database/migrations/2023_08_17_140705_tasks.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->tinyInteger('fulfilled')->default(0);
+            $table->text('description')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
