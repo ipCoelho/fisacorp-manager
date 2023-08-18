8ecc5fcad081438a7fc0eb72115fe92fb4f38a93
Israel Coelho <ipcoelho.business@gmail.com>
2023-08-18 12:51:26 -0300

    chore/env: Removed template files and old code, also adapted some files

--- manager-front/src/router/index.jsx
+++ manager-front/src/router/index.jsx
@@ -1,15 +1,10 @@
-import { useAuth } from '../hooks/AuthContext';
 import PrivateRoutes from './private';
 import PublicRoutes from './public';
 
 function Routes() {
-  const { user, globalLoading, handleValidateToken } = useAuth();
-
   const token = localStorage.getItem('token');
 
-  handleValidateToken(token);
-
-  return <>{user ? <PrivateRoutes /> : <PublicRoutes />}</>;
+  return <>{token ? <PrivateRoutes /> : <PublicRoutes />}</>;
 }
 
 export default Routes;
